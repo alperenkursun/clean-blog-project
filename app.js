c189fa7325808cdeab12f6d33a9181852efc3306
alperenkursun <alperenkursun@outlook.com>
2023-02-14 01:32:00 +0300

    public and views folder added

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+let ejs = require("ejs");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/addnew", function (req, res) {
+  res.render("add_post");
+});
+
+app.get("/post", function (req, res) {
+  res.render("post");
 });
 
 port = 3001;
